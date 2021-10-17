0d6351f800527c82ab359d9b5fc813d54cafee6c
Mario <marionovak5@gmail.com>
2021-10-17 22:10:17 +0200

    Tests: passing and checking BigNumber values

--- test/index.ts
+++ test/index.ts
@@ -1,5 +1,6 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
+import { BigNumber } from "@ethersproject/bignumber";
 
 describe("NetworkSummary", function () {
   it("Sets the proper creator address", async function () {
@@ -18,12 +19,14 @@ describe("NetworkSummary", function () {
     const networkSummary = await NetworkSummary.deploy();
     await networkSummary.deployed();
 
-    // Day ID
-    const dayId = 27;
+    // Input
+    const dayId = BigNumber.from("27");
+    const blocks = BigNumber.from("349");
+    const gas = BigNumber.from("849");
 
     // Execute method with non-creator user
     await expect(
-      networkSummary.connect(guest).addSummary(dayId, 349, 849)
+      networkSummary.connect(guest).addSummary(dayId, blocks, gas)
     ).to.be.revertedWith('WriteAccessForbiden("' + guest.address + '")');
   });
   it("Stores summary for the day and returns it", async function () {
@@ -31,17 +34,37 @@ describe("NetworkSummary", function () {
     const networkSummary = await NetworkSummary.deploy();
     await networkSummary.deployed();
 
-    // Day ID
-    const dayId = 27;
+    // Input
+    const dayId = BigNumber.from("27");
+    const blocks = BigNumber.from("349");
+    const gas = BigNumber.from("849");
 
-    const addSummaryTx = await networkSummary.addSummary(dayId, 349, 849);
+    const addSummaryTx = await networkSummary.addSummary(dayId, blocks, gas);
     // wait until the transaction is mined
     await addSummaryTx.wait();
 
     const getSummaryTx = await networkSummary.getSummary(dayId);
 
-    // https://ethereum.stackexchange.com/questions/89423/convert-hex-number-from-solidity/89430
-    expect(getSummaryTx[0].toString()).to.equal((349).toString());
-    expect(getSummaryTx[1].toString()).to.equal((849).toString());
+    expect(getSummaryTx[0]).to.equal(blocks);
+    expect(getSummaryTx[1]).to.equal(gas);
+  });
+  it("Stores summary with BigNumber gas", async function () {
+    const NetworkSummary = await ethers.getContractFactory("NetworkSummary");
+    const networkSummary = await NetworkSummary.deploy();
+    await networkSummary.deployed();
+
+    // Input
+    const dayId = BigNumber.from("27");
+    const blocks = BigNumber.from("349");
+    const gas = BigNumber.from("84900000000000000000000");
+
+    const addSummaryTx = await networkSummary.addSummary(dayId, blocks, gas);
+    // wait until the transaction is mined
+    await addSummaryTx.wait();
+
+    const getSummaryTx = await networkSummary.getSummary(dayId);
+
+    expect(getSummaryTx[0]).to.equal(blocks);
+    expect(getSummaryTx[1]).to.equal(gas);
   });
 });
